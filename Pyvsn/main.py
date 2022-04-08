388c2fb5b85b6f3c1a7e1b6a110459af967e185b
MeTeoRise <teoleo777@hotmail.com>
2022-04-08 14:03:59 +0300

    Python Server Class starting implementation

---
+++ Pyvsn/main.py
@@ -0,0 +1,6 @@
+import server.server as server
+
+if __name__ == "__main__":
+
+    server = server.Server()
+    server.start_server()
---
+++ Pyvsn/server/server.py
@@ -0,0 +1,30 @@
+import socket
+
+
+class Server:
+    port = 8866
+    host = "localhost"
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def server_activate(self):
+        self.socket.bind((self.host, self.port))
+        self.socket.listen()
+
+    def get_request(self):
+        return self.socket.accept()
+
+    def start_server(self):
+        self.server_activate()
+        conn, addr = self.get_request()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+    def server_close(self):
+        self.socket.close()
